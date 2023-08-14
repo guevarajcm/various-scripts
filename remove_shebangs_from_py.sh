cfea874e72109f99d0924c6ffe683ff306d31a67
guevarajcm <104320360+guevarajcm@users.noreply.github.com>
2023-08-13 21:35:27 -0600

    No comments in code

--- remove_shebangs_from_py.sh
+++ remove_shebangs_from_py.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 for file in *.py; do
-    # Si la primera línea es un shebang
     if head -n 1 "$file" | grep -q "^#!"; then
-        # Crea un archivo temporal sin la primera línea
         tail -n +2 "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
     fi
 
-    # Si la primera línea está vacía
     if [[ $(head -n 1 "$file") == "" ]]; then
-        # Crea un archivo temporal sin la primera línea
         tail -n +2 "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
     fi
 done
